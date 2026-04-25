ab536beaa49568f8bf8889f6b4e7e44bb044ccd9
Ankool Srivastava <ankoolsrivastava@example.com>
2026-04-25 23:23:47 +0530

    BAsic MAths

---
+++ basicMaths/PrimeNo.java
@@ -0,0 +1,31 @@
+package basicMaths;
+
+import java.util.Scanner;
+
+public class PrimeNo {
+
+    static void checkPrime(int n) {
+
+        for (int i = 2; i <= n; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        checkPrime(num);
+    }
+}
